a27addf9f988a465202aac663d2181d6f67ac002
Daus Hirana <55246175+Hira478@users.noreply.github.com>
2021-06-24 20:24:09 +0700

    some comment

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
+//server main body
 const init = async () => {
   const server = Hapi.server({
-    port: 5000,
+    port: 5000, //change ENV between prod and local
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.32.66',
     routes: {
       cors: {
@@ -13,7 +14,8 @@ const init = async () => {
   });
 
   server.route(routes);
-
+  
+  //server starter
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
